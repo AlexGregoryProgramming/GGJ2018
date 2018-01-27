ef505e2aab690c35c69797fd1bec818e0adb6078
KNOWN-UNIVERSE\rocvarga <Ozzy@uat.edu>
2018-01-26 20:13:45 -0700

    Ozzy and Rocky things

---
+++ GGJ2018/Assets/Ozzy Scripts/Battery.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : MonoBehaviour {
+
+	Transform tf;
+
+	// Use this for initialization
+	void Start () {
+		tf = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			tf.position += Vector3.forward * Time.deltaTime;
+		}
+	}
+}
